a9345e0d86b50ebf63a1575491c9c36aa085705d
platinumplace <warlyn200@gmail.com>
2023-01-16 01:13:23 -0400

    hidden tokens

--- config/zoho.php
+++ config/zoho.php
@@ -3,8 +3,8 @@
 return [
     "url_token" => "https://accounts.zoho.com/oauth/v2/token",
     "url_api" => "https://www.zohoapis.com/crm/v2/",
-    "client_id" => "1000.7FJQ4A2KDH9S2IJWDYL13HATQFMA2H",
-    "client_secret" => "c3f1d0589803f294a7c5b27e3968ae1658927da9d7",
+    "client_id" => "1000.XXXXXXXXXXXXXXXXXXXXXXXXXXXX",
+    "client_secret" => "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
     "grant_type" => "refresh_token",
-    "refresh_token" => "1000.54a92a6561b3369b3b82a785773c0d49.6d11ba02de999c09d4f3971a4d7b9aae",
+    "refresh_token" => "XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX",
 ];
