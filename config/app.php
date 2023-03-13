8d0608066081d84219d21790b8ff4fa6c455ade6
platinumplace <warlyn200@gmail.com>
2023-03-12 20:32:14 -0400

    update packages

--- config/app.php
+++ config/app.php
@@ -197,6 +197,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        PlatinumPlace\LaravelZohoCrmApi\ZohoServiceProvider::class,
     ],
 
     /*
@@ -212,6 +213,7 @@ return [
 
     'aliases' => Facade::defaultAliases()->merge([
         // 'ExampleClass' => App\Example\ExampleClass::class,
+        'Zoho' => PlatinumPlace\LaravelZohoCrmApi\Facades\Zoho::class,
     ])->toArray(),
 
 ];
---
+++ config/zoho.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    "url_token" => env("ZOHO_URL_TOKEN"),
+    "url_api" => env("ZOHO_URL_API"),
+    "redirect_uri" => env("ZOHO_REDIRECT_URI"),
+    "client_id" => env("ZOHO_CLIENT_ID"),
+    "client_secret" => env("ZOHO_CLIENT_SECRET"),
+    "grant_type" => env("ZOHO_GRANT_TYPE"),
+    "refresh_token" => env("ZOHO_REFRESH_TOKEN"),
+];
