6a7c2c47063dce0211684d4dc2d8daebe312b13f
PlatinumPlace <warlyn200@gmail.com>
2023-07-01 18:42:05 -0400

    update seeders

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -16,14 +16,5 @@ class UserSeeder extends Seeder
             'name' => 'admin',
             'email' => 'admin@gnb.com',
         ]);
-
-        // colonial user
-        \App\Models\Users\User::factory()->create([
-            'name' => 'admin',
-            'email' => 'colonial@gnb.com',
-            'account_name_id' => '3222373000013452591',
-            'account_name' => 'LA COLONIAL, S.A',
-            'contact_name_id' => '3222373000031472977',
-        ]);
     }
 }
