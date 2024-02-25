4e879c4fdc914dd88595469e191c5dee4e06a804
PaltinumPlace <warlyn200@gmail.com>
2024-02-25 16:48:08 -0400

    update

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // admin user
+        \App\Models\User::factory()->create([
+            'name' => 'admin',
+            'email' => 'admin@gnb.com',
+        ]);
+    }
+}
